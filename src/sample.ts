11121cf4a19c8bda1d7f8954f3f689ab4abbd674
ditinagrawal <ditin.agrawal05@gmail.com>
2025-08-17 16:13:05 +0530

    new design

---
+++ src/sample.ts
@@ -0,0 +1,10 @@
+const author = {
+  name: `Ditin Agrawal`,
+  nickname: `ditin.A`,
+  x: `https://x.com/ditinagrawal`,
+  linkedin: `https://www.linkedin.com/in/ditinagrawal/`,
+  github: `https://github.com/ditinagrawal`,
+  email: "mailto:ditin.agrawal05@gmail.com",
+};
+
+export { author };
