1a3afef6c2722f04f5a2c11025b6129515582447
ditinagrawal <ditin.agrawal05@gmail.com>
2025-12-15 00:50:53 +0530

    the beginning

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import { ViewTransitions } from "next-view-transitions";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Ditin Agrawal - A Full Stack Developer",
+  description:
+    "Ditin Agrawal is a full stack web developer with a passion for creating beautiful and functional websites.",
+  icons: {
+    icon: "/avatar.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ViewTransitions>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </ViewTransitions>
+  );
+}
