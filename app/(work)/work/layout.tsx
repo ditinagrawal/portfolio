e481ea38899946adf9961af29049939078cab387
ditinagrawal <ditin.agrawal05@gmail.com>
2025-12-16 13:33:44 +0530

    add work route

---
+++ app/(work)/work/layout.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "@/components/shared/footer";
+import { Header } from "@/components/shared/header";
+import { Container } from "@/components/ui/container";
+
+export default function WorkLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="pt-16">
+      <Header />
+      <Container>{children}</Container>
+      <Footer />
+    </main>
+  );
+}
